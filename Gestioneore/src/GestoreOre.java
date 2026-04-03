53e72c6452a9b394f26bb36a58a54398684d047f
Miriam <153954154+bayloncina@users.noreply.github.com>
2026-04-03 17:13:48 +0200

    Fix mostraOreLavorate

--- Gestioneore/src/GestoreOre.java
+++ Gestioneore/src/GestoreOre.java
@@ -30,9 +30,11 @@ public class GestoreOre {
 
             switch (scelta) {
                 case 1 -> inserisciOre(dipendenti, scanner);
-                case 2 -> { }
-                case 3 -> { }
-                case 4 -> mostraOreLavorate();
+                case 2 -> {
+                }
+                case 3 -> {
+                }
+                case 4 -> mostraOreLavorate(dipendenti, scanner);
                 case 0 -> {
                     running = false;
                     System.out.println("Chiusura del programma");
@@ -44,6 +46,7 @@ public class GestoreOre {
         scanner.close();
     }
 
+    // inserisci Ore
     private void inserisciOre(ArrayList<Dipendente> dipendenti, Scanner scanner) {
         System.out.println("Seleziona dipendente: ");
         for (int i = 0; i < dipendenti.size(); i++) {
@@ -69,23 +72,28 @@ public class GestoreOre {
         int ore = dip.getOreLavorateGiornalmente();
         totaleOre = totaleOre + ore;
     }
-    
-
-    private void mostraOreLavorate() {
+//Mostra ore lavorate per dipendente
+    private void mostraOreLavorate(ArrayList<Dipendente> dipendenti, Scanner scanner) {
     if (dipendenti.isEmpty()) {
         System.out.println("Nessun dipendente presente.");
         return;
     }
 
-    System.out.println("\nOre Lavorate");
-    for (Dipendente dip : dipendenti) {
-        String tipo = (dip instanceof DipendenteFullTime) ? "Full Time" : "Part Time";
-        System.out.println("Nome: " + dip.getNome() + " " + dip.getCognome());
-        System.out.println("Mansione: " + dip.getMansione());
-        System.out.println("Azienda: " + dip.getAzienda());
-        System.out.println("Tipo: " + tipo);
-        System.out.println("Ore giornaliere: " + dip.getOreLavorateGiornalmente());
+    System.out.println("Seleziona dipendente:");
+    for (int i = 0; i < dipendenti.size(); i++) {
+        System.out.println(i + " - " + dipendenti.get(i).getNome());
     }
-    System.out.println("Totale ore accumulate: " + totaleOre);
+
+    int scelta = Integer.parseInt(scanner.nextLine());
+    Dipendente dip = dipendenti.get(scelta);
+
+    String tipo = (dip instanceof DipendenteFullTime) ? "Full Time" : "Part Time";
+    System.out.println("\n===== ORE LAVORATE =====");
+    System.out.println("Nome: " + dip.getNome() + " " + dip.getCognome());
+    System.out.println("Mansione: " + dip.getMansione());
+    System.out.println("Azienda: " + dip.getAzienda());
+    System.out.println("Tipo: " + tipo);
+    System.out.println("Ore giornaliere: " + dip.getOreLavorateGiornalmente());
+    System.out.println("Totale ore lavorate: " + dip.getTotaleOreLavorate());
 }
 }
