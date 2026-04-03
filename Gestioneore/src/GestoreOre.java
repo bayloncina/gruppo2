434e6cf145140c9e727f5af41014b6c45bec4e80
Miriam <153954154+bayloncina@users.noreply.github.com>
2026-04-03 16:43:53 +0200

    metodoAggiung

--- Gestioneore/src/GestoreOre.java
+++ Gestioneore/src/GestoreOre.java
@@ -4,10 +4,50 @@ import java.util.Scanner;
 
 public class GestoreOre {
 
-    // private ArrayList<Dipendente> dipendenti = new ArrayList<>();
+    private ArrayList<Dipendente> dipendenti = new ArrayList<>();
     int totaleOre;
 
-    private void inserisciOre(ArrayList<Dipendente> dipendenti, Scanner scanner) {
+    
+    public void menu() {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n===== GESTIONALE ORE =====");
+            System.out.println("1. Inserisci ore");
+            System.out.println("2. Modifica ore inserite");
+            System.out.println("3. Elimina ore inserite");
+            System.out.println("4. Mostra ore lavorate");
+            System.out.println("0. Esci");
+            System.out.print("Scelta: ");
+
+            int scelta = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (scelta) {
+                case 1 -> {
+                inserisciOre(dipendenti, scanner);
+                }
+                case 2 -> {
+
+                }
+                case 3 -> {
+                }
+                case 4 -> {
+
+                }
+                case 0 -> {
+                    running = false;
+                    System.out.println("Chiusura del programma");
+                }
+                default -> System.out.println("Scelta non valida.");
+            }
+        }
+
+        scanner.close();
+    }
+private void inserisciOre(ArrayList<Dipendente> dipendenti, Scanner scanner) {
 
         System.out.println("Seleziona dipendente: ");
         for (int i = 0; i < dipendenti.size(); i++) {
@@ -34,46 +74,6 @@ public class GestoreOre {
         // totale ore
         int ore = dip.getOreLavorateGiornalmente();
         totaleOre = totaleOre + ore;
-    }
-
-    public void menu() {
-
-        Scanner scanner = new Scanner(System.in);
-        boolean running = true;
-
-        while (running) {
-            System.out.println("\n===== GESTIONALE ORE =====");
-            System.out.println("1. Inserisci ore");
-            System.out.println("2. Modifica ore inserite");
-            System.out.println("3. Elimina ore inserite");
-            System.out.println("4. Mostra ore lavorate");
-            System.out.println("0. Esci");
-            System.out.print("Scelta: ");
-
-            int scelta = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (scelta) {
-                case 1 -> {
-
-                }
-                case 2 -> {
-
-                }
-                case 3 -> {
-                }
-                case 4 -> {
-
-                }
-                case 0 -> {
-                    running = false;
-                    System.out.println("Chiusura del programma");
-                }
-                default -> System.out.println("Scelta non valida.");
-            }
-        }
-
-        scanner.close();
     }
 
 }
