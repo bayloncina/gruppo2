a9d8a3b5fa1e176077057c12258527b03db603d5
LucaMB <lmbaratti@gmail.com>
2026-04-03 15:36:00 +0200

    classe persona

--- Gestioneore/src/Persona.java
+++ Gestioneore/src/Persona.java
@@ -1,3 +1,20 @@
-public class Persona {
-    
+import java.util.ArrayList;
+
+abstract class Persona {
+
+    private String id;
+    private int contatore = 0;
+    private String nome;
+    private String cognome;
+
+    public Persona(String nome, String cognome) {
+        if (nome == null || nome.trim().isEmpty()) {
+            this.nome = "Utente sconosciuto";
+        } else {
+            this.nome = nome;
+        }
+        this.cognome = cognome;
+        this.id = String.format("ID-%03d", ++contatore);
+    }
+
 }
